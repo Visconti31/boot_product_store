8ad837126795100195ce201fed95ccee39464050
Joao Pedro Portella Guedes Visconti <JOVSI@vestas.com>
2024-12-06 13:26:20 +0000

    Set POST end point to create products

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,36 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
+import Product from './models/product.model.js'
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello, World!')
+})
+
+app.post('/api/products', async (req, res) => {
+  const product = req.body // Grab information from user body sent
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide all fields' })
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save()
+    res.status(201).json({ success: true, data: newProduct })
+  } catch (error) {
+    console.error(`Error in creating product: ${error.message}`)
+    res.status(500).json({ success: false, message: 'Server Error' })
+  }
 })
 
 app.listen(5000, () => {
